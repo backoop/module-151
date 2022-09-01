fb3103ca7794d129dc278e5491a27687c8b849a2
Janis Ringli <janis.ringli@gmail.com>
2022-09-01 16:37:09 +0200

    working database pogChamp

---
+++ module-151/02_JDBC_ODBC/janis/jokesdb/src/main/java/org/example/App.java
@@ -0,0 +1,34 @@
+package org.example;
+
+
+import java.sql.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        try {
+// Driver class für MySql-Datenbank laden Class.forName("com.mysql.cj.jdbc.Driver");
+// Verbindung zur Datenbank herstellen
+// Setzen Sie den richtigen Port, auf dem ihre Datenbank läuft // zbsp 3306
+            String connectionUrl = "jdbc:mysql://localhost:3306/jokes";
+//beachten Sie username und passwort
+            Connection connection = DriverManager.getConnection(connectionUrl, "root", "1234");
+// Sql Befehl aufbauen und ausführen
+            Statement stmt = connection.createStatement();
+            ResultSet entries = stmt.executeQuery("SELECT * FROM joke");
+// alle Datensätze aus dem ResultSet auslesen und // (Schritt 1) in einer ArrayList ablegen
+            while (entries.next()) {
+                System.out.println(entries.getString("text"));
+            }
+// alle verwendeten Objekte schliessen
+            entries.close();
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
