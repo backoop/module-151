993147a9148e827212b34679e82d813e6d3b975c
LorisMueller <lomue@gmx.ch>
2022-09-15 15:54:00 +0200

    jokedb update

--- module-151/02_JDBC_ODBC/loris/jokedbconnect2/src/main/java/ch/bbw/lm/App.java
+++ module-151/02_JDBC_ODBC/loris/jokedbconnect2/src/main/java/ch/bbw/lm/App.java
@@ -13,8 +13,8 @@ public class App
 
         try {// Driver class für MySql-Datenbankladen
             Class.forName("com.mysql.cj.jdbc.Driver");// Verbindung zur Datenbank herstellen// Setzen Sie den richtigen Port, auf dem ihre Datenbank läuft// zbsp 3306
-            String connectionUrl = "jdbc:mysql://localhost:3306/Joke";//beachten Sie username und passwort
-            Connection connection = DriverManager.getConnection(connectionUrl, "root", "1234");// Sql Befehl aufbauen und ausführen
+            String connectionUrl = "jdbc:mysql://localhost:3306/joke";//beachten Sie username und passwort
+            Connection connection = DriverManager.getConnection(connectionUrl, "root", "");// Sql Befehl aufbauen und ausführen
             Statement stmt = connection.createStatement();
             ResultSet entries = stmt.executeQuery("SELECT * FROM joke");// alle Datensätze aus dem ResultSet auslesen und// (Schritt 1) in einer ArrayList ablegen
             while (entries.next()) {
